b74810c5efaaf462a48ac7f19340c162cbd08721
BACompton <compton.boyd@gmail.com>
2016-09-13 22:48:19 -0400

    Added code for task 1-3
    This includes a serial connection interface, an interface for iRobot
    Create 2, and a program that does the following:
    - When the power button is pressed and the robot is stopped, move along a
      square with 250 mm sides.
    - When the power button is pressed and the robot is moving, stop the
      robot.
    
    All code has also been committed.

---
+++ robot_inf.py
@@ -0,0 +1,247 @@
+#!/usr/bin/python
+
+import struct
+
+import serial_inf
+
+# =============================================================================
+#                   Constants for iRobot Create 2
+# =============================================================================
+
+_BAUD_RATE = 115200     # iRobot Create 2 Default baud rate
+_TIMEOUT = 1            # The default read timeout to avoid indefinite blocking
+_SENSORS_OPCODE = "142" # Opcode for the sensors command
+_BYTE = 0xFF            # Helps isolate a byte
+_BYTE_SIZE = 8          # The number of bit in a byte of iRobot Create 2
+
+class State:
+    """ Represents the different states of the iRobot Create 2. The value at
+        each state is the necessary command to enter that state.
+
+        There is no command to transition back to the START state, so the
+        transition from the START state is one way.
+    """
+    START = ""
+    RESET = "7"
+    STOP = "173"
+    PASSIVE = "128"
+    SAFE = "131"
+    FULL = "132"
+
+class Button:
+    """ Represents the different buttons on the iRobot Create 2. The value of
+        each button are the corresponding bit it refers to in the button packet.
+
+        This also contains the packet id, and the number of data bytes to read.
+    """
+    # Buttons
+    CLOCK = 0x80
+    SCHEDULE = 0x40
+    DAY = 0x20
+    HOUR = 0x10
+    MINUTE = 0x08
+    DOCK = 0x04
+    SPOT = 0x02
+    CLEAN = 0x01
+
+    # Packet Information
+    PACKET_ID = "18"
+    DATA_BYTES = 1
+
+class Drive:
+    """ Represents special cases for the radius of the iRobot Create 2's
+        drive command, and the bounds for velocity and radius.
+    """
+    # Special Cases for the radius
+    STRAIGHT = 0x8000
+    STRAIGHT_ALT = 0x7FFF
+    TURN_CW = 0xFFFF
+    TURN_CCW = 0x0001
+
+    # Bounds established by iRobot Create 2's OI specifications.
+    MAX_VEL = 500
+    MIN_VEL = -500
+    MAX_RAD = 2000
+    MIN_RAD = -2000
+
+# =============================================================================
+#                       iRobot Create 2's Interface
+# =============================================================================
+
+class Robot:
+    """ Represents a interface for iRobot Create 2 over a serial connection.
+
+        Attributes:
+            state: A State value used to indicate the robot's current state.
+    """
+    state = None
+
+    _serial_conn = None
+
+    def __init__(self, port, buad=_BAUD_RATE, timeout=_TIMEOUT, start=True):
+        """ Initializes a robot by first establishing the serial connection to
+            the robot. and then sending the start command the robot. This
+            command will set the robot's mode to PASSIVE, and grant the
+            ability to send other command.
+
+        :param port:
+            The serial port to the robot
+        :param buad:
+            The buad rate of the robot
+        :param timeout:
+            The read timeout for the robot
+        :param start:
+            Flags the robot to enter the PASSIVE state. If this is false,
+            the robot's state will need to be changed from START to
+            PASSIVE before any other commands can be sent.
+        :return:
+        """
+        self._serial_conn = serial_inf.SerialConn(port, buad, timeout)
+        self.state = State.START
+
+        if start:
+            self.change_state(State.PASSIVE)
+
+    def change_state(self, new_state):
+        """ Changes the robot into the provided state provided it is a different
+            state and the new state is not State.START.
+
+        :type new_state State:
+        :param new_state:
+            The state that the robot should transition into.
+        :return:
+            True if the change was successful, otherwise false.
+        """
+        if new_state != self.state and new_state != State.START:
+            self._serial_conn.send_command(new_state)
+            self.state = new_state
+            return True
+        return False
+
+    def drive(self, velocity, radius):
+        """ Issues the drive command for the robot. This method specifically
+            calls the drive command with opcode 137 which is only available
+            to a robot is the SAFE or FULL state.
+
+            Both arguments will be interrupt as 16 bit values in two's
+            complement representation. This means this method supports
+            explicit integer values(i.e. -1, 500, -150) and 16 bit
+            integers(i.e 0xFFFF. 0x7FFF).
+
+            All the special cases for this command can be found in the Drive
+            class.
+        :param velocity:
+            The velocity of the robot in millimeters per second. This can
+            range from -500 to 500 mm/s.
+        :param radius:
+            The radius the robot should turn in mm. This can range from
+            -2000 to 2000 mm.
+        """
+        # Ensures the drive command is available.
+        if self.state != State.SAFE and self.state != State.FULL:
+            print "Drive Command Unavailable"   # Look into error checking and raise error
+            return
+
+        # Bounds the velocity and radius to the robot's specified range
+        bound_vel = self._convert_bound(velocity, Drive.MIN_VEL, Drive.MAX_VEL)
+        # Captures the special cases that fall outside the bounds for radius
+        if radius == Drive.STRAIGHT or radius == Drive.STRAIGHT_ALT:
+            bound_rad = radius
+        else:
+            bound_rad = self._convert_bound(radius, Drive.MIN_RAD, Drive.MAX_RAD)
+
+        # Separates and prepares the data for encoding
+        data = (bound_vel >> _BYTE_SIZE & _BYTE,
+                bound_vel & _BYTE,
+                bound_rad >> _BYTE_SIZE & _BYTE,
+                bound_rad & _BYTE)
+
+        # Sends drive command to robot
+        self._serial_conn.send_command("137 %s %s %s %s" % data)
+
+    def _convert_bound(self, value, lower_bound, upper_bound):
+        """ This begins by converting the provided value into a 16 bit two's
+            complement integer. Next, it bounds the converted integer between
+             the provided upper and lower bounds.
+
+        :param value:
+            The value to bound.
+        :param lower_bound:
+            The minimum value this value can possesses.
+        :param upper_bound:
+            The maximum value this value can possesses.
+        :return:
+            The bounded version of the value which is based on the upper and
+            lower bounds.
+        """
+        # Converts value to 16 bit two's complement integer via bitwise.
+        most_sig_bit = 0x8000
+
+        # Gets the two least significant bits
+        convert_val = value & _BYTE << _BYTE_SIZE | value & _BYTE
+        # Extends the most significant bit if it is a 1. This is done by
+        # carrying out the most significant bit.
+        if bool(convert_val & most_sig_bit):
+            convert_val = convert_val | ~(_BYTE << _BYTE_SIZE | _BYTE)
+
+        # Bounds the converted value
+        if convert_val > upper_bound:
+            return upper_bound
+        elif convert_val < lower_bound:
+            return lower_bound
+        return convert_val
+
+    def read_button(self, button):
+        """ Reads the provided button's bit from the Buttons packet. This method
+            is available to a robot in the PASSIVE, SAFE, or FULL state.
+
+        :type button Button:
+        :param button:
+            The button to read
+        :return:
+            The boolean value of the corresponding button.
+        """
+        data = self._read_packet(Button.PACKET_ID, Button.DATA_BYTES)
+
+        # Gets first byte
+        byte = struct.unpack("B", data)[0]
+        return bool(byte & button)
+
+    def read_buttons(self):
+        """ Reads all the buttons on the iRobot Create 2. This method
+            is available to a robot in the PASSIVE, SAFE, or FULL state.
+
+        :return:
+            A dictionary of all the buttons' values. Each record is
+            addressed by the value in Button. Thus, the individual button
+            values can be acquired by like so:
+                clean = someRobot.read_buttons()[Button.CLEAN]
+        """
+        data = self._read_packet(Button.PACKET_ID, Button.DATA_BYTES)
+
+        # Gets first byte
+        byte = struct.unpack("B", data)[0]
+        return {
+            Button.CLEAN: bool(byte & Button.CLEAN),
+            Button.SPOT: bool(byte & Button.SPOT),
+            Button.DOCK: bool(byte & Button.DOCK),
+            Button.MINUTE: bool(byte & Button.MINUTE),
+            Button.HOUR: bool(byte & Button.HOUR),
+            Button.DAY: bool(byte & Button.DAY),
+            Button.SCHEDULE: bool(byte & Button.SCHEDULE),
+            Button.CLOCK: bool(byte & Button.CLOCK)
+        }
+
+    def _read_packet(self, packet_id, data_bytes):
+        """ Sends the sensor command with the provided packet id to the robot
+            and reads the robots response.
+
+        :param packet_id:
+            The packet id of the desired packet
+        :param data_bytes:
+            The number of bytes the robot will respond with
+        :return:
+            The raw data that the robot responded with.
+        """
+        self._serial_conn.send_command(_SENSORS_OPCODE+" "+packet_id)
+        return self._serial_conn.read_data(data_bytes)
---
+++ square.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+
+import threading
+import time
+
+import serial_inf
+import robot_inf
+
+# =============================================================================
+#                       Main program for project 1
+# =============================================================================
+
+class DriveControl(threading.Thread):
+    """
+        This performs the action sequence. In this case, it will move in a
+        square with 250 mm.
+
+        In more detail, this is where the low-level actuator controls are
+        held. The robot will only respond to the actuator commands if it is
+        in SAFE or FULL mode.
+    """
+    _robot = None
+    _stop = None
+
+    def __init__(self, thread_id, name, counter, robot):
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.name = name
+        self.counter = counter
+        self._stop = False
+        self._robot = robot
+
+    def run(self):
+        turns = 3               # The number of turns left to make.
+        side_len = 250          # The length of the square in mm.
+        vel = 50                # Robot's velocity in mm/s.
+        turn_time = 3.20        # The time it take to turn 90 degrees in seconds.
+
+        # The amount each interval should wait before
+        # checking for the stop condition again. In seconds.
+        wait_interval = 0.015
+
+        while not self._stop and turns >= 0:
+            # Drive forward
+            self._robot.drive(vel, robot_inf.Drive.STRAIGHT)
+            # Time to run the command
+            if self._wait(abs(side_len/vel), wait_interval):
+                break
+
+            # Turn 90 degrees
+            self._robot.drive(vel, robot_inf.Drive.TURN_CW)
+            # Time it takes to turn
+            if self._wait(turn_time, wait_interval):
+                break   # could also be 'continue'
+            turns -= 1
+
+        # Stops robot
+        self._robot.drive(0,0)
+
+    def stop(self):
+        """
+            Sets the flag to stop the thread to True. The thread will
+            not immediately stop. Instead, the thread will exit safely.
+        """
+        self._stop = True
+
+    def _wait(self, wait_time, interval):
+        """ Internal method that divides the time in between actuator
+            commands into small intervals. This enables the ability
+            to check for the stopping condition while running an action.
+
+        :param wait_time:
+            The total amount of time to wait
+        :param interval:
+            The amount of time for a single interval.
+        :return:
+            True if the stopping condition was detected, otherwise false.
+        """
+        time_left = wait_time
+
+        while time_left > 0:
+            # Tell the caller it needs to stop
+            if self._stop:
+                return True
+
+            # Wait another time interval
+            interval_time = interval
+            if interval_time > time_left:
+                interval_time = time_left
+            time_left -= interval_time
+            time.sleep(interval_time)
+        return False
+
+
+def robot_controller():
+    """
+        This listens for the button press. If the clean button is pressed,
+        this will decide if the robot should stop or start its action sequence.
+
+        In this case the action sequence is to make a square with 250 mm sides.
+        The robot will start if the action sequence is not running. The robot
+        will stop if the action sequence is running.
+    """
+
+    port_list = serial_inf.list_serial_ports()
+
+    if len(port_list) > 1:
+        print "Requires a serial connection."
+        return -1
+
+    # The State space for the robot is the constants defined in
+    # robot_inf.State (change to is thread running)
+
+    robot = robot_inf.Robot(port_list[0])     # Serial connection to robot
+    robot.change_state(robot_inf.State.SAFE)
+    print "Connected to robot"
+
+    act_control = None                  # Low-level actuator control
+    release = True
+
+    print "Listening for press"
+    while True:
+        # High-Level State Action
+        if robot.read_button(robot_inf.Button.CLEAN):
+            # Only allows the initial press perform an action.
+            if not release:
+                continue
+
+            # Start Square
+            release = False
+            if act_control is None:
+                act_control = DriveControl(3, "Drive-Controls-Square", 1, robot)
+                act_control.start()
+
+            # Stop Driving
+            else:
+                act_control.stop()
+                act_control = None
+        else:
+            release = True
+
+        # Clocks while loop to the update rate of the iRobot Create 2.
+        time.sleep(.015)
+    print "Stopping Listening"
+
+if __name__ == '__main__':
+    robot_controller()
